629193dbf112f9cad771851db6e1ab6c26fe4537
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2021-11-02 01:27:53 +0100

    feat: add kubernetes hero with animation

---
+++ src/components/KubernetesHero.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="relative grid gap-6 grid-cols-1 place-items-center text-white">
+    <img
+      class="w-40 h-40 z-10"
+      src="/kubernetes-logo.svg"
+      alt="Kubernetes logo"
+    />
+    <div class="absolute z-0">
+      <CloudIcon class="w-24 h-24 animate-clouds-slow-delayed text-gray-300" />
+      <CloudIcon class="w-24 h-24 animate-clouds-normal text-gray-300" />
+      <CloudIcon class="w-24 h-24 animate-clouds-fast text-gray-300" />
+    </div>
+    <div class="absolute z-0">
+      <CloudIcon class="w-24 h-24 animate-clouds-fast-delayed" />
+      <CloudIcon class="w-24 h-24 animate-clouds-normal" />
+      <CloudIcon class="w-24 h-24 animate-clouds-slow" />
+    </div>
+    <h2 class="text-3xl font-black z-10">Let's get started</h2>
+    <p class="text-semibold text-center z-10">
+      Kubernetes is a scalable container-based cloud system, ready to run your
+      applications in a fault tolerant way, but you don't have any yet.
+    </p>
+    <button class="border-2 rounded-lg px-3 py-2 z-10">
+      Launch my first cluster
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { CloudIcon, CloudDownloadIcon } from '@heroicons/vue/solid';
+</script>
--- src/pages/kubernetes.vue
+++ src/pages/kubernetes.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="relative bg-primary md:pt-32 pb-32 pt-12">
-    <div class="px-4 md:px-10 mx-auto w-full">
+    <div class="px-4 md:px-10 mx-auto w-full overflow-x-hidden">
       <teleport to="#page-title">
         <h1>Kubernetes clusters</h1>
       </teleport>
+
+      <div>
+        <KubernetesHero />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useTitle } from '@vueuse/core';
+import KubernetesHero from '../components/KubernetesHero.vue';
 
 useTitle('Kubernetes - Civo.com');
 </script>
